7fc7cb7f337da319f4f49760b05f303272cb33e3
Guilherme Sales <guilhermedesaales@gmail.com>
2025-07-02 15:11:25 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ import pymysql
 # Mude o user e senha do BD aqui 
 
 DB_HOST = "localhost"
-DB_USER = "user"
-DB_PASS = "pass"
+DB_USER = "root"
+DB_PASS = "root"
 DB_NAME = "db_maracujina"
 DB_PORT = 3306
 
